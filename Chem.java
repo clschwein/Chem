8c31eb52fb0545b74f8053a0d0fbc88826eae1c5
clschwein <schwein@vt.edu>
2013-02-05 15:20:33 -0500

    Input changes

--- Chem.java
+++ Chem.java
@@ -1,3 +1,23 @@
+// On my honor:
+//
+// - I have not used source code obtained from another student,
+// or any other unauthorized source, either modified or
+// unmodified.
+//
+// - All source code and documentation used in my program is
+// either my original work, or was derived by me from the
+// source code published in the textbook for this course.
+//
+// - I have not discussed coding details about this project with
+// anyone other than my partner (in the case of a joint
+// submission), instructor, ACM/UPE tutors or the TAs assigned
+// to this course. I understand that I may discuss the concepts
+// of this program with other students, and that another student
+// may help me debug my program so long as neither of us writes
+// anything during the discussion or modifies any computer file
+// during the discussion. I have violated neither the spirit nor
+// letter of this restriction.
+
 import java.util.Arrays;
 import java.io.*;
 
@@ -14,15 +34,20 @@ public class Chem {
 	// Integers for starting values
 	private static int numSpecies = 0;
 	private static int numReactions = 0;
+	private static int numDisplay = 0;
 	private static int simulationTime = 0;
 	private static int numRuns = 0;
 	private static int time = 0;
-	private Reaction current;
+	private static boolean track = false;
+	private static Reaction current = null;
+	private static BufferedWriter out = null;
 
 	// Data structures for species/reaction info
 	private static int[] species = null;
-	private static int[] tracks = null;
-	private static MinHeap<Reaction> reactions = null;
+	private static int[] displays = null;
+	private static boolean[] tracks = null;
+	private static Reaction[] reactionsArray = null;
+	private static MinHeap<Reaction> reactionsHeap = null;
 
 	/**
 	 * Main method to run simulation.
@@ -39,22 +64,30 @@ public class Chem {
 			System.exit(1);
 		}
 
-		// Read in the initial values from the input file
-		if (!readFile(args[0])) {
+		// Read from file to initialize variables
+		if (!initialize(args[1])) {
 			System.exit(2);
 		}
 
-		numRuns = args[1];
+		try {
+			out = new BufferedWriter(new FileWriter(args[2]));
+		} catch (FileNotFoundException e) {
+			System.out.println("The output file could not be found.");
+			System.exit(3);
+		}
+		
+		numRuns = Integer.parseInt(args[0]);
 
 		for (int i = 0; i < numRuns; i++) {
 			time = 0;
-
+			
+			// TODO: set propensities, next time, and add to heap for all reactions
+			
 			while (time < simulationTime) {
 				track = false;
 				
 				// Choose the next reaction
-				current = reactions.getMin();
-				current.fire();
+				current = reactionsHeap.getMin();
 				time += current.getNextTime();
 				
 				// Test for simulation end
@@ -66,7 +99,7 @@ public class Chem {
 				if (current.getReactants() != null) {
 					for (int index : current.getReactants()) {
 						species[index]--;
-						if (tracked(index)) {
+						if (tracks[index]) {
 							track = true;
 						}
 					}
@@ -76,7 +109,7 @@ public class Chem {
 				if (current.getProducts() != null) {
 					for (int index : current.getProducts()) {
 						species[index]++;
-						if (tracked(index)) {
+						if (tracks[index]) {
 							track = true;
 						}
 					}
@@ -86,13 +119,14 @@ public class Chem {
 				for (Reaction rxn : current.getTable()) {
 					double propensity = rxn.getRate();
 					int[] reactants = rxn.getReactants();
+					ReactionType type = rxn.getType();
 					
-					if (rxn instanceof RxnTwo) {
+					if (type == ReactionType.RXN_TWO) {
 						propensity *= species[reactants[0]];
-					} else if (rxn instanceof RxnThree) {
+					} else if (type == ReactionType.RXN_THREE) {
 						propensity *= species[reactants[0]];
 						propensity *= (species[reactants[0]] - 1);
-					} else if (rxn instanceof RxnFour) {
+					} else if (type == ReactionType.RXN_FOUR) {
 						propensity *= species[reactants[0]];
 						propensity *= species[reactants[1]];
 					}
@@ -107,38 +141,30 @@ public class Chem {
 					}
 					
 					// Remove and re-add to re-order heap
-					reactions.remove(rxn);
-					reactions.add(rxn);
+					reactionsHeap.remove(rxn);
+					reactionsHeap.add(rxn);
 				}
 				
 				// Output for reaction
 				if (numRuns == 1 && track) {
-					if (!trackOutput(args[2])) {
-						System.exit(3);
-					}
+					trackOutput();
 				}
 			}
 			
 			// Output summary data for one of many runs
 			if (numRuns > 1) {
-				if (!runOutput(args[2])) {
-					System.exit(4);
-				}
+				runOutput();
 			}
 		}
 
 		// Output summary data for a single run
 		if (numRuns == 1) {
-			if (!singleOutput(args[2])) {
-				System.exit(5);
-			}
+			singleOutput();
 		}
 		
 		// Output summary data for multiple runs
 		if (numRuns > 1) {
-			if (!finalOutput(args[2])) {
-				System.exit(6);
-			}
+			finalOutput();
 		}
 	}
 
@@ -149,7 +175,7 @@ public class Chem {
 	 *            the file name for reading
 	 * @return true if successful, false otherwise
 	 */
-	private static boolean readFile(String fileName) {
+	private static boolean initialize(String fileName) {
 		
 		BufferedReader in = null;
 
@@ -157,7 +183,7 @@ public class Chem {
 		try {
 			in = new BufferedReader(new FileReader(fileName));
 		} catch (FileNotFoundException e) {
-			System.out.println("The file could not be found.");
+			System.out.println("The input file could not be found.");
 			return false;
 		}
 
@@ -165,15 +191,16 @@ public class Chem {
 			String line = in.readLine();
 			String[] tokens = line.split(" ");
 
-			// Use the first three numbers as initial values
+			// Use the first four numbers as initial values
 			numSpecies = Integer.parseInt(tokens[0]);
 			numReactions = Integer.parseInt(tokens[1]);
-			simulationTime = Integer.parseInt(tokens[2]);
+			numDisplay = Integer.parseInt(tokens[2]);
+			simulationTime = Integer.parseInt(tokens[3]);
 
 			// Allocate arrays based on our given parameters
 			species = new int[numSpecies];
-			reactions = new int[numReactions][];
-			rates = new double[numReactions];
+			reactionsArray = new Reaction[numReactions];
+			displays = new int[numDisplay];
 
 			line = in.readLine();
 			tokens = line.split(" ");
@@ -182,27 +209,75 @@ public class Chem {
 				species[i] = Integer.parseInt(tokens[i]);
 			}
 
+			line = in.readLine();
+			tokens = line.split(" ");
+			// Read in display species
+			for (int i = 0; i < numDisplay; i++) {
+				displays[i] = Integer.parseInt(tokens[i]);
+				tracks[Integer.parseInt(tokens[i])] = true;
+			}
+			
 			// Read in reaction coefficients
 			for (int i = 0; i < numReactions; i++) {
 				line = in.readLine();
 				tokens = line.split(" ");
 
-				reactions[i] = new int[tokens.length - 1];
+				int offset = 0;
+				double rate = 0;
+				ReactionType type = null;
+				int[] reactants = null;
+				int[] products = null;
 
-				// Input reaction coefficients
-				for (int j = 0; j < 2; j++) {
-					reactions[i][j] = Integer.parseInt(tokens[j]);
+				// Input reactant coefficients
+				if (tokens[0].startsWith("->")) {
+					type = ReactionType.RXN_ONE;
+					reactants = null;
+					offset = 0;
+				} else if (tokens[1] == "+") {
+					type = ReactionType.RXN_FOUR;
+					reactants = new int[2];
+					reactants[0] = Integer.parseInt(tokens[0].substring(1));
+					reactants[1] = Integer.parseInt(tokens[2].substring(1));
+					offset = 3;
+				} else if (tokens[0].charAt(0) == '2') {
+					type = ReactionType.RXN_THREE;
+					reactants = new int[2];
+					reactants[0] = Integer.parseInt(tokens[0].substring(2));
+					reactants[1] = reactants[0];
+					offset = 1;
+				} else {
+					type = ReactionType.RXN_TWO;
+					reactants = new int[1];
+					reactants[0] = Integer.parseInt(tokens[0].substring(1));
+					offset = 1;
 				}
 
 				// Input reaction rate
-				rates[i] = Double.parseDouble(tokens[2]);
+				rate = Double.parseDouble(tokens[offset].substring(2));
+				offset ++;
 
 				// Input product coefficients
-				for (int j = 3; j < tokens.length; j++) {
-					reactions[i][j - 1] = Integer.parseInt(tokens[j]);
+				if (offset == tokens.length) {
+					products = null;
+				} else if ((offset == tokens.length - 1) && tokens[offset].charAt(0) == '2') {
+					products = new int[2];
+					products[0] = Integer.parseInt(tokens[offset].substring(2));
+					products[1] = products[0];
+				} else if (offset == tokens.length - 1) {
+					products = new int[1];
+					products[0] = Integer.parseInt(tokens[offset].substring(1));
+				} else {
+					products = new int[2];
+					products[0] = Integer.parseInt(tokens[offset].substring(1));
+					products[1] = Integer.parseInt(tokens[offset + 2].substring(1));
 				}
+				
+				// Add reaction to array
+				reactionsArray[i] = new Reaction(rate, type, reactants, products);
 			}
 
+			// TODO: build reaction dependent tables from reaction array
+			
 			in.close();
 		} catch (IOException e) {
 			System.out.println("Error reading from file.");
@@ -218,22 +293,40 @@ public class Chem {
 	/**
 	 * Output tracked species when changed
 	 * 
-	 * @param fileName
-	 *            the file name for reading
 	 * @return true if successful, false otherwise
 	 */
-	private static boolean trackOutput(String fileName) {
+	private static void trackOutput() {
 
+		// TODO: implement
 	}
 	
 	/**
-	 * Final output for the summmary of multiple runs
+	 * Output for one run of many
 	 * 
-	 * @param fileName
-	 *            the file name for reading
 	 * @return true if successful, false otherwise
 	 */
-	private static boolean finalOutput(String fileName) {
+	private static void runOutput() {
 
+		// TODO: implement
+	}
+	
+	/**
+	 * Output for the summary of a single run
+	 * 
+	 * @return true if successful, false otherwise
+	 */
+	private static void singleOutput() {
+
+		// TODO: implement
+	}
+	
+	/**
+	 * Final output for the summary of multiple runs
+	 * 
+	 * @return true if successful, false otherwise
+	 */
+	private static void finalOutput() {
+
+		// TODO: implement
 	}
 }
